abd64dd27dec6ee16bb0c74b2c78fd4b4c7eb60e
Danybucce <daniele.bucce26@gmail.com>
2025-10-13 17:38:07 +0200

    aggiornato user service con tutte le operazioni crud

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
 import {Repository} from 'typeorm';
 import {User} from './users.entity';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { METHODS } from 'http';
 
 @Injectable()
 export class UserService{
@@ -11,8 +12,25 @@ export class UserService{
     ) {}
 
     createUser(firstName: string, lastName: string, email: string, password_hash: string, birth_date: Date,  phone: string, role: string){
-        const user = this.userRepository.create()
-        this.userRepository.save
-        return user
+        const user = this.userRepository.create();
+        this.userRepository.save;
+        return user;
+    }
+    findAllUsers(): Promise<User[]>{
+        return this.userRepository.find();
+    }
+    async findUserById(id:number): Promise<User>{
+         const user = await this.userRepository.findOne({where:{id}});
+         if(!user){
+            throw new NotFoundException('utente non trovato');
+         }
+        return user;
+    }
+    updateUser(id:number): Promise<User>{
+        const user = this.userRepository.update;
+        return this.findUserById(id);
+    } 
+    deleteUser(id:number){
+        this.userRepository.delete;
     }
 }
